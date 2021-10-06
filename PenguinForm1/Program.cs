bf9a832f76a91163709686efd631f1b6bc241b6e
Peter Blanchfield <zygomy@gmail.com>
2021-10-06 16:09:01 +0100

    changed the form name and text

--- PenguinForm1/Program.cs
+++ PenguinForm1/Program.cs
@@ -17,7 +17,7 @@ namespace PenguinForm1
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new penguinForm1());
         }
     }
 }
